43693254c8dbccffbf66565b5db6a3c1915bdd90
Ly Nate <njl36@zoo.cs.yale.edu>
2024-10-03 12:04:43 -0400

    innit

--- proj3/optimizer.c
+++ proj3/optimizer.c
@@ -2,9 +2,14 @@
 
 
 void Optimizer(NodeList *funcdecls) {
-/*
-*************************************
-     TODO: YOUR CODE HERE
-*************************************
-*/
+    // run through list
+    while(funcdecls != NULL){
+        Node *node = funcdecls->node;
+        // get expressions 
+        // if expression check if we can fold
+        funcdecls = funcdecls->next;
+    }
+// constant folding (solve expressions a = 3+ 5, a = 8)
+//constant propogation  (replace vars c = b/a c = 16/8)
+// eliminate dead code (removes lines where var innit but not used)
 }
